269ac2c111abb45a4df2bbf0d18c01bba140f39e
Bevenkatesh <44633417+Bevenkatesh@users.noreply.github.com>
2018-10-31 12:56:03 +0530

    Add files via upload

---
+++ sketch_sep01a.ino
@@ -0,0 +1,69 @@
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
+
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "capam-93f7b.firebaseio.com"
+#define FIREBASE_AUTH "jDX7IUSHcRPb9pRHBMeeIVLvqNrwQmgwcmjz7mm8"
+#define WIFI_SSID "fasaakk"
+#define WIFI_PASSWORD "jaibalayya"
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(D0,OUTPUT);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+
+
+void loop() {
+
+String a = Firebase.getString("Led");
+
+Serial.println(a);
+
+if (a=="1"){
+digitalWrite(D0,HIGH);
+}
+else
+{
+  digitalWrite(D0,LOW);
+}
+
+delay(1000);
+
+
+
+
+}
